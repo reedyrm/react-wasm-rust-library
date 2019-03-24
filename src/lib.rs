3bba7c28647a1aeb00cae9e5019a9f06ed408918
reedyrm <richard.g.reedy@gmail.com>
2019-03-24 13:43:20 -0500

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+mod utils;
+
+use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
+
+cfg_if::cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, react-wasm-rust-library!");
+}
+
+#[wasm_bindgen]
+pub fn hello() {
+    println!("Hello, world!");
+}
+
+#[wasm_bindgen]
+pub fn add(num1: i64, num2: i64) -> i64 {
+    num1 + num2
+}
+
+#[wasm_bindgen]
+pub fn sub(num1: i64, num2: i64) -> i64 {
+    num1 - num2
+}
